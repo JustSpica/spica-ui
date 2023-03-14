004e21ae9f9c95ab3e9022b234751bfb60b81b07
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2023-03-14 01:46:52 -0300

    feat: created color tokens documentation

---
+++ packages/docs/src/pages/components/ColorsGrid.tsx
@@ -0,0 +1,22 @@
+import { getContrast } from 'polished'
+import { colors } from '@ignite-ui/tokens'
+
+export function ColorsGrid() {
+  return Object.entries(colors).map(([key, color]) => {
+    return (
+      <div key={key} style={{ backgroundColor: color, padding: '2rem' }}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            fontFamily: 'monospace',
+            color: getContrast(color, '#FFF') < 3.5 ? '#2d3032' : '#FFF',
+          }}
+        >
+          <strong>${key}</strong>
+          <span>{color}</span>
+        </div>
+      </div>
+    )
+  })
+}
