fd19f4e642a57a3d2c3f10f2d90237d9a0a3f626
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2023-03-15 02:08:09 -0300

    feat: created new variation to Checkbox component with label

--- packages/docs/src/stories/Checkbox.stories.tsx
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -1,10 +1,44 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Checkbox, CheckboxProps } from '@ignite-ui/react'
+import { Box, Checkbox, CheckboxProps, Text } from '@ignite-ui/react'
 
 export default {
   title: 'Form/Checkbox',
   component: Checkbox,
-  args: {},
+  args: {
+    id: 'checkbox_story$1',
+  },
+  argTypes: {
+    id: {
+      table: {
+        disable: true,
+      },
+    },
+  },
 } as Meta<CheckboxProps>
 
 export const Default: StoryObj<CheckboxProps> = {}
+
+export const Label: StoryObj<CheckboxProps> = {
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          htmlFor="checkbox_story$1"
+          css={{
+            display: 'flex',
+            alignItems: 'center',
+            flexDirection: 'row-reverse',
+            gap: '$2',
+            cursor: 'pointer',
+          }}
+        >
+          <Text size="sm" css={{ lineHeight: 0 }}>
+            Checkbox label
+          </Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+}
--- packages/react/src/components/Box.tsx
+++ packages/react/src/components/Box.tsx
@@ -2,7 +2,10 @@ import React, { ComponentProps } from 'react'
 
 import { BoxStyled } from '../styles/Box'
 
-export interface BoxProps extends ComponentProps<typeof BoxStyled> {}
+export interface BoxProps extends ComponentProps<typeof BoxStyled> {
+  as?: React.ElementType
+  htmlFor?: string
+}
 
 export function Box({ children, ...props }: BoxProps) {
   return <BoxStyled {...props}>{children}</BoxStyled>
