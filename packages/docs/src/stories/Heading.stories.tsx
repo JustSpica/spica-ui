918f92a9a53f1fb064d87bffe77556fbf9d62fb0
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2023-03-15 01:22:50 -0300

    fix: fixed argTypes of Heading story

--- packages/docs/src/stories/Heading.stories.tsx
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -5,14 +5,44 @@ export default {
   title: 'Typography/Heading',
   component: Heading,
   args: {
-    children: 'Example Title',
+    as: 'h1',
+    size: 'md',
   },
 } as Meta<HeadingProps>
 
-export const Default: StoryObj<HeadingProps> = {}
+export const Default: StoryObj<HeadingProps> = {
+  args: {
+    children: 'H1 Heading Title',
+  },
+  argTypes: {
+    as: {
+      table: {
+        disable: true,
+      },
+    },
+    size: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+}
 export const CustomTag: StoryObj<HeadingProps> = {
   args: {
     children: 'Custom Title',
-    as: 'h1',
+  },
+  argTypes: {
+    as: {
+      options: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+      control: {
+        type: 'select',
+      },
+    },
+    size: {
+      options: ['sm', 'md', 'lg', '2xl', '3xl', '4xl', '5xl', '6xl'],
+      control: {
+        type: 'select',
+      },
+    },
   },
 }
